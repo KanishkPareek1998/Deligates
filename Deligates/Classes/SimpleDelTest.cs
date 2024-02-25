cae93ec542d20c7149541650f6af9f3160050018
Kanishk <Kanishk@DESKTOP-EC5BBEJ>
2024-02-25 22:19:52 +0530

    Add project files.

---
+++ Deligates/Classes/SimpleDelTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deligates.Classes
+{
+    public delegate void MyDelegate(string message);
+    public class SimpleDelTest
+    {
+        public MyDelegate SimapleDeligate = new MyDelegate(PrintMessage);
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
---
+++ Deligates/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Deligates.Classes;
+Console.WriteLine("Application has been started.!!!"); 
+SimpleDelTest simpleDel = new SimpleDelTest();
+Console.WriteLine("Calling Simple Deligate");
+simpleDel.SimapleDeligate("This is Message passed to Simple Deligate");
