92c95872661f547093bce3780e2eb4bce8e51c52
KanishkPareek1998 <kanishk7568@gmail.com>
2024-02-25 23:05:51 +0530

    1.Created examples for MultiCast Deligates.

---
+++ Deligates/Classes/MultiCastDel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deligates.Classes
+{
+    public class MultiCastDel
+    {
+        public delegate void Operation(int x, int y);
+        private static Operation delegate1 = Add;
+        private static Operation delegate2 = Subtract;
+        public Operation MultiCastDeligate = delegate1+ delegate2;
+        public Operation getMultiCastDeligate()
+        {
+            return MultiCastDeligate;
+        }
+        static void Add(int x, int y)
+        {
+            Console.WriteLine($"Addition: {x + y}");
+        }
+
+        static void Subtract(int x, int y)
+        {
+            Console.WriteLine($"Subtraction: {x - y}");
+        }
+    }
+}
--- Deligates/Program.cs
+++ Deligates/Program.cs
@@ -4,3 +4,23 @@ Console.WriteLine("Application has been started.!!!");
 SimpleDelTest simpleDel = new SimpleDelTest();
 Console.WriteLine("Calling Simple Deligate");
 simpleDel.SimapleDeligate("This is Message passed to Simple Deligate");
+Console.WriteLine("-------------------------------------------------------");
+Console.WriteLine("Calling Multi Cast Deligate");
+MultiCastDel multiCastDel = new MultiCastDel();
+Console.WriteLine("Enter first integer");
+string s1 = Console.ReadLine();
+Console.WriteLine("Enter second integer");
+string s2 = Console.ReadLine();
+int x;
+int y;
+while (!int.TryParse(s1, out x))
+{
+    Console.WriteLine("Invalid input. Please enter a valid number:");
+    s1 = Console.ReadLine();
+}
+while (!int.TryParse(s2, out y))
+{
+    Console.WriteLine("Invalid input. Please enter a valid number:");
+    s2 = Console.ReadLine();
+}
+multiCastDel.MultiCastDeligate(x, y);
